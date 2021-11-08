add4ce0ce14e4adac9331d89ff41f10515b91fb6
xTommyBoy <jsqhfg12@gmail.com>
2021-11-08 18:32:56 +0100

    Ajout de la partie front du product

---
+++ front/js/product.js
@@ -0,0 +1,98 @@
+const api_url = "http://localhost:3000/api/products";
+const itemAddToCart = document.querySelector("#addToCart");
+// récupération de l'item panier pour le modifier 
+let cart = localStorage.getItem("cart");
+
+// check pour regarder si un panier est déja existant sinon passage a l'etape suivante
+if (cart) {
+  cart = JSON.parse(cart);
+} else {
+  cart = [];
+}
+
+function getUrlProductId() {
+  // récupération des id via url
+  const urlParam = new URLSearchParams(window.location.search);
+  return urlParam.get("id");
+}
+
+async function getProductData(id) {
+  // récupération des données par l'ID du produit
+  const data = await fetch(api_url + "/" + id).then(function (data) {
+    return data.json();
+  });
+  return data;
+}
+function updateProductHTML(product) {
+  // création de l'html en fonction du produit
+  const itemImage = document.querySelector(".item__img");
+  const itemTitle = document.querySelector("#title");
+  const itemPrice = document.querySelector("#price");
+  const itemDescription = document.querySelector("#description");
+  const itemColors = document.querySelector("#colors");
+  const itemQuantity = document.querySelector("#quantity");
+
+  itemImage.innerHTML =
+    '<img src="' + product["imageUrl"] + '" alt="' + product["altTxt"] + '">';
+  itemTitle.innerHTML = product["name"];
+  itemPrice.innerHTML = product["price"];
+  itemDescription.innerHTML = product["description"];
+
+  product.colors.forEach(function (color) {
+    itemColors.innerHTML +=
+      '<option value="' + color + '">' + color + "</option>";
+  });
+  itemQuantity.value = "1";
+}
+
+function addItemToCart(product) {
+  // ajout du produit dans le panier
+  const itemColors = document.querySelector("#colors");
+  const itemQuantity = document.querySelector("#quantity");
+  const cartItem = {
+    _id: product["_id"],
+    altTxt: product["altTxt"],
+    imageUrl: product["imageUrl"],
+    name: product["name"],
+    price: product["price"],
+    color: itemColors.value,
+    quantity: parseInt(itemQuantity.value),
+  };
+  if (cartItem.color == "") {
+    alert("SVP, choisissez une couleur");
+    return false;
+  }
+
+  const itemIndex = cart.findIndex(function (item) {
+    return item["_id"] === cartItem["_id"];
+  });
+
+  if (itemIndex === -1) {
+    cart.push(cartItem);
+  } else {
+    colorindex = cart.findIndex(function (item) {
+      return (
+        item["_id"] === cartItem["_id"] && item["color"] === cartItem["color"]
+      );
+    });
+
+    if (colorindex === -1) {
+      cart.push(cartItem);
+    } else {
+      cart[colorindex].quantity += cartItem.quantity;
+    }
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+window.onload = async function () {
+  // récupération des données générales du produit avec un check 
+  const id = getUrlProductId();
+  const product = await getProductData(id);
+
+  // actualisation de l'html en fonction du produit
+  updateProductHTML(product);
+  itemAddToCart.onclick = function () {
+    addItemToCart(product);
+  };
+};
--- front/js/script.js
+++ front/js/script.js
@@ -2,35 +2,35 @@ const api_url = "http://localhost:3000/api/products";
 const itemsElements = document.getElementById("items");
 
 async function getProductsData() {
-  // appel de l'api pour obtenir les données des produits
+  // appel de l'api pour obtenir les données des products
   let data = await fetch(api_url).then(function (data) {
     return data.json();
   });
   return data;
 }
-function generateProductHTML(produit) {
-  // utilisation des données du produit pour generer le code html du produit
-  let productHTML = '<a href="./product.html?id=' + produit["_id"] + '">';
+function generateProductHTML(product) {
+  // utilisation des données du product pour generer le code html du product
+  let productHTML = '<a href="./product.html?id=' + product["_id"] + '">';
   productHTML +=
     '<article> <img src="' +
-    produit["imageUrl"] +
+    product["imageUrl"] +
     '" alt="' +
-    produit["altTxt"] +
+    product["altTxt"] +
     '">';
-  productHTML += '<h3 class="productName">' + produit["name"] + "</h3>";
+  productHTML += '<h3 class="productName">' + product["name"] + "</h3>";
   productHTML +=
-    '<p class="productDescription">' + produit["description"] + "</p>";
+    '<p class="productDescription">' + product["description"] + "</p>";
   productHTML += "</article></a>";
   return productHTML;
 }
 
 window.onload = async function () {
-  // recuperation de toutes les données des produits en question
-  let produits = await getProductsData();
+  // recuperation de toutes les données des products en question
+  let products = await getProductsData();
 
-  produits.forEach(function (produit) {
+  products.forEach(function (product) {
     // affichage de l'html sur la page
-    let html = generateProductHTML(produit);
+    let html = generateProductHTML(product);
     itemsElements.innerHTML += html;
   });
 };
